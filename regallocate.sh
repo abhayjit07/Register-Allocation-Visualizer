08c8dd19b7b931892f2bdb3dcc83c1f0293cd34f
Srinivasa R <srinivasa.r0503@gmail.com>
2023-05-26 13:47:50 +0530

    added pipeline bash script

---
+++ regallocate.sh
@@ -0,0 +1,19 @@
+# take filename as command line arg
+
+if [ $# -ne 3 ]
+then
+    echo "Usage: $0 filename ir_filename num_of_registers"
+    exit 1
+fi
+
+if [ ! -f $1 ]
+then
+    echo "File $1 not found!"
+    exit 2
+fi
+
+#generate IR
+./IR\ Generation/IR $1 -o $2
+
+#run liveness script on ir
+python liveness.py $2 $3
